f2480f613383114ed7e74d30f3792f8d507673d7
Arno <u21457451@tuks.co.za>
2024-04-23 14:00:59 +0200

    Added basic test case

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,4 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-
+// Basic test to display sign in page
+test('renders sign in page', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Sign In/i);
+  expect(linkElement).toBeInTheDocument();
+});
