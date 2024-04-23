a5d47e88f60cb293d712686b25008ffffa3425c7
Arno <u21457451@tuks.co.za>
2024-04-23 23:50:06 +0200

    Added Google OAuth

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import './App.css';
 
 // components
 import Signin from './components/signin/Signin';
+import Signout from './components/signout/Signout';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleSuccessfulLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleSuccessfulLogout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <Fragment>
-      <Signin />
+      {!isLoggedIn && <Signin onSuccessfulLogin={handleSuccessfulLogin} />}
+      {isLoggedIn && <Signout onSuccessfulLogout={handleSuccessfulLogout} />}
     </Fragment>
   );
 }
--- client/src/components/signin/Signin.js
+++ client/src/components/signin/Signin.js
@@ -1,25 +1,39 @@
-import React, { Fragment } from "react";
+import React from "react";
+import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from 'jwt-decode';
 
-const Signin = () => {
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
+const Signin = ({ onSuccessfulLogin } ) => {
   return (
-    <Fragment>
-      <div className="bg-gradient-to-b from-yahoo-purple to-white h-screen w-full">
-        <div className="flex justify-center items-center h-full">
-          <div className="bg-white rounded-2xl shadow-2xl p-5 w-1/3">
-            <div className="flex flex-col items-center">
-              <h1 className="text-4xl font-bold text-yahoo-purple">
-                yahoo<text className="text-yahoo-purple italic">!</text>
-              </h1>
-                          <h2 className="text-2xl font-bold mt-10">Sign in</h2>
-                          <h3 className="text-lg  mt-10">Continue with Google</h3>
-              <button className="bg-yahoo-purple text-white py-2 px-4 rounded-full mt-2">
-                Google
-              </button>
+    <div className="bg-gradient-to-b from-yahoo-purple to-white h-screen w-full">
+      <div className="flex justify-center items-center h-full">
+        <div className="bg-white rounded-2xl shadow-2xl p-5 w-1/3">
+          <div className="flex flex-col items-center">
+            <h1>
+              <span className="text-4xl font-bold text-yahoo-purple">
+                yahoo<span className="text-yahoo-purple italic">!</span>
+              </span>
+            </h1>
+            <h2 className="text-2xl font-bold mt-10">Sign in</h2>
+            <h3 className="text-lg  mt-10">Continue with Google</h3>
+            <div id="signInButton" className="py-2 px-4 rounded-full mt-2">
+              <GoogleLogin
+                clientId={clientId}
+                onSuccess={(credentialResponse) => {
+                  const decoded = jwtDecode(credentialResponse.credential);
+                  console.log(decoded);
+                  onSuccessfulLogin(decoded);
+                }}
+                onError={() => {
+                  console.log("Login Failed");
+                }}
+              />
             </div>
           </div>
         </div>
       </div>
-    </Fragment>
+    </div>
   );
 };
 
